da556095a464934d96e0d31bd2f503018c1304bf
Richard Palacios G <apalaciosg91@gmail.com>
2022-04-06 13:38:48 -0500

    feat(products): mock api request of products Load product list with useEffect

--- react_client/src/app/screens/Homescreen.tsx
+++ react_client/src/app/screens/Homescreen.tsx
@@ -1,7 +1,29 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import { CircularProgress, Flex, Heading, Stack } from "@chakra-ui/react";
 import React from "react";
+import api from "~/product/api";
+import { Product } from "~/product/types";
 
 const HomeScreen: React.FC = () => {
+  const [products, setProducts] = React.useState<Product[]>([]);
+  const [status, setStatus] = React.useState<
+    "pending" | "resolved" | "rejected"
+  >("pending");
+
+  React.useEffect(() => {
+    api.list().then((products) => {
+      setProducts(products);
+      setStatus("resolved");
+    });
+  }, []);
+
+  if (status === "pending") {
+    return (
+      <Flex alignItems="center" justifyContent="center" paddingY={12}>
+        <CircularProgress isIndeterminate color="primary.500" />
+      </Flex>
+    );
+  }
+
   return (
     <Stack flex={1} spacing={6}>
       <Heading color="gray.400" fontSize="4xl">
